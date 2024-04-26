23f63a10c3c9b16c089b32eaf1e27757f08673fe
Michael Drag <mdrag@OBC-MDRAG.local>
2024-04-25 22:23:09 -0400

    update

---
+++ assets/script.js
@@ -0,0 +1,59 @@
+const userName = document.querySelector('#username');
+const titleName = document.querySelector('#title');
+const contentText = document.querySelector('#content');
+const submitButton = document.querySelector('#button');
+
+
+let userNameArray = []; 
+let titleNameArray = [];
+let contentTextArray = [];
+
+submitButton.addEventListener('click', function (event) {
+  event.preventDefault();
+
+  userNameArray.push(userName.value.trim()); //
+  titleNameArray.push(titleName.value.trim());
+  contentTextArray.push(contentText.value.trim());
+
+  let blogPost = {
+    userName: userNameArray, //using trim will remove unwanted spaces
+    titleName: titleNameArray,
+    contentText: contentTextArray,
+
+  };
+
+  if (userName.value === '') {
+    alert('error', 'Username cannot be blank'); //fix if statement
+  } 
+  if (titleName.value === '') {
+    alert('error', 'Title cannot be blank');
+  } 
+  if (contentText.value === '') {
+    alert('error', 'Content cannot be blank');
+  } 
+  if (userName.value && titleName.value && contentText.value) {
+    window.location.href = "/Users/mdrag/Desktop/Bootcamp/challenge-04/assets/blogposts.html" // linking to the second HTML page
+  }
+
+
+  localStorage.setItem("Username", JSON.stringify(blogPost.userName));
+  localStorage.setItem("Title", JSON.stringify(blogPost.titleName));
+  localStorage.setItem("Content", JSON.stringify(blogPost.contentText));
+  
+
+
+
+
+});
+
+
+/* for (let i = 0; i < 5; i++) {
+  // Create a new div element
+  const divElement = document.createElement('div');
+  // Set the content of the div, you can change this according to your array structure
+  divElement.textContent = userNameArray[i % userNameArray.length];
+  // Append the div element to the body (you can change this to any other parent element)
+  document.body.appendChild(divElement);
+}
+*/
+
---
+++ assets/script2.js
@@ -0,0 +1,37 @@
+const userNameComplete = document.getElementById('user-complete');
+const titleComplete = document.getElementById('title-complete');
+const contentComplete = document.getElementById('content-complete');
+const backButton = document.querySelector('#back-button');
+const themeButtonEl = document.querySelector('#theme-button');
+const bodyEl = document.querySelector ('#body');
+
+let isDark = true;
+
+themeButtonEl.addEventListener('click', function () { 
+    if (isDark) {
+        bodyEl.style.backgroundColor = "#d9e9e8";
+        bodyEl.style.color = "#1a1a1a";
+        isDark = !isDark;
+    } else {
+        bodyEl.style.backgroundColor = "#1a1a1a";
+        bodyEl.style.color = "#d9e9e8";
+        isDark = !isDark;
+    }
+  });
+
+backButton.addEventListener('click', function (event) {
+    event.preventDefault();
+    window.location.href = "/Users/mdrag/Desktop/Bootcamp/challenge-04/index.html" 
+  })
+
+function saveUserResponses() { 
+    let userLocal = localStorage.getItem("Username");
+    let titleLocal = localStorage.getItem("Title");
+    let contentLocal = localStorage.getItem("Content");
+    userNameComplete.textContent = JSON.parse(userLocal);
+    titleComplete.textContent = JSON.parse(titleLocal);
+    contentComplete.textContent = JSON.parse(contentLocal);
+    return
+  }
+
+  saveUserResponses();
